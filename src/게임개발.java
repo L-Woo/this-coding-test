739f523766989e93ed354c816126dd277e4521a8
ss6116@daum.net <ss6116@daum.net>
2023-03-23 02:55:43 +0900

    구현 게임개발 완성

--- src/게임개발.java
+++ src/게임개발.java
@@ -14,21 +14,9 @@ public class 게임개발 {
     // 2. 캐릭터의 바로 왼쪽 방향에 아직 가보지 않은 칸이 존재한다면, 왼쪽으로 회전만 수행하고 1단계로 돌아간다.
     // 3. 만약 네 방향 모두 이미 가본 칸이거나 바다로 되어 있는 칸인 경우에는, 바라보는 방향을 유지한 채로 한 칸 뒤로 가고 1단계로 돌아간다.
     // 이때 뒤쪽 방향이 바다인 칸이라 뒤로 갈 수 없는 경우에는 움직임을 멈춘다.
-    static class Player {
-        int x, y, dir;
-
-        public Player(int x, int y, int dir) {
-            this.x = x;
-            this.y = y;
-            this.dir = dir;
-        }
-
-    }
 
     static int N, M;
     static int[][] map, visited;
-    // 0 : 북 / 1 : 동 / 2 : 남 / 3 : 서
-    static int[] dir = {0, 1, 2, 3};
     static int[] dr = {-1, 0, 1, 0};
     static int[] dc = {0, 1, 0, -1};
 
@@ -47,8 +35,8 @@ public class 게임개발 {
         int x = Integer.parseInt(st.nextToken());
         int y = Integer.parseInt(st.nextToken());
         int dir = Integer.parseInt(st.nextToken());
-
-        Player p = new Player(x, y, dir);
+        int dirCount = 0;
+        int count = 1;
 
         for(int i = 0; i < N; i ++) {
             st = new StringTokenizer(br.readLine());
@@ -59,18 +47,8 @@ public class 게임개발 {
 
 
 
-    }
-
-    static int move(Player p) {
-
-        int x = p.x;
-        int y = p.y;
-        int dir = p.dir;
-        int dirCount = 0;
-        int count = 1;
-
         visited[x][y] = 1;
-        
+        // 0 : 북 / 1 : 동 / 2 : 남 / 3 : 서
         while (true) {
 
             if(dir - 1 >= 0) {
@@ -81,32 +59,34 @@ public class 게임개발 {
 
             int nx = x + dr[dir];
             int ny = y + dc[dir];
-            if(nx >= 0 && nx < N && ny >= 0 && ny < M) {
+            if(nx >= 0 && ny >= 0 && nx < N && ny < M) {
                 if(map[nx][ny] == 0 && visited[nx][ny] == 0) {
                     visited[nx][ny] = 1;
                     count++;
                     dirCount = 0;
                     x = nx;
                     y = ny;
-                }else {
-                    dirCount++;
+                    continue;
                 }
-
-                if(dirCount == 4) {
+            }else {
+                dirCount++;
+            }
+            if(dirCount == 4) {
+                if(nx >= 0 && ny >= 0 && nx < N && ny < M) {
                     nx = x - dr[dir];
                     ny = y - dc[dir];
-
-                    if(map[nx][ny] == 1) {
-                        return count;
+                    if(map[nx][ny] == 0){
+                        x = nx;
+                        y = ny;
                     }
-
                 }else {
-
+                    break;
                 }
+                dirCount = 0;
             }
 
-
         }
+        System.out.println(count);
     }
 
 }
